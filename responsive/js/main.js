ddd7ae3d060070ae0e1de53c7c65b97b03872be6
komaryk <iryna.komarenets@gmail.com>
2017-02-14 19:47:15 +0200

    Start making  responsive

---
+++ responsive/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	// Open and close select box on click
+    $(".select").on("click", function(){
+        $(this).toggleClass("select--open");
+    });
+
+    //Close select box on click outside of the box
+ //    $(document).on('click', function(event) {
+	//     if (!$(event.target).is('.select--open')) {
+	//         $('.select').toggleClass('select--open');
+	//     }
+	// });
+	$(document).mouseup(function (e)
+	{
+	    var container = $(".select");
+
+	    if (!container.is(e.target) // if the target of the click isn't the container
+	        && container.hasClass('select--open') == true) // and the container is open
+	    {
+	        $(container).removeClass('select--open');
+	    }
+	});
+});
