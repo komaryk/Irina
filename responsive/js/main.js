9f94064273d358590486fbb116173651ddcbc48f
komaryk <iryna.komarenets@gmail.com>
2017-02-16 17:18:45 +0200

    Fix select boxes

--- responsive/js/main.js
+++ responsive/js/main.js
@@ -3,9 +3,20 @@ $(document).ready(function(){
     $(".select").on("click", function(){
         $(this).toggleClass("select--open");
     });
-
+    $(".select").hover(function(){
+    }, function(){
+        $(this).removeClass("select--open");
+    });
+    $(".select li").on("click", function(){
+    	var value = $(this).data().value;
+        $(this).parent().find(".js-select").text(value).data().value = value;
+    });
     $(".header").on("click", function(){
         $(this).toggleClass("header--open");
+    });
+    $(".header").hover(function(){
+    }, function(){
+        $(this).removeClass("header--open");
     });
 
     //Close select box on click outside of the box
@@ -14,14 +25,14 @@ $(document).ready(function(){
 	//         $('.select').toggleClass('select--open');
 	//     }
 	// });
-	$(document).mouseup(function (e)
-	{
-	    var container = $(".select");
+	// $(document).on("click",function (e)
+	// {
+	//     var container = $(".select");
 
-	    if (!container.is(e.target) // if the target of the click isn't the container
-	        && container.hasClass('select--open') == true) // and the container is open
-	    {
-	        $(container).removeClass('select--open');
-	    }
-	});
+	//     if (!container.is(e.target) // if the target of the click isn't the container
+	//         && container.hasClass('select--open') == true) // and the container is open
+	//     {
+	//         $(container).removeClass('select--open');
+	//     }
+	// });
 });
