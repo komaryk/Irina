bb117d59c30578ed5eddf833432fd876e9bfa743
komaryk <iryna.komarenets@gmail.com>
2017-02-22 22:09:42 +0200

    Add gradient bg

---
+++ jquery basics/gradient/main.js
@@ -0,0 +1,18 @@
+var angle = 0;
+var speed = $("#range").val();
+// Set the gradient parameters
+$(document).ready(function(e){
+    function gradient() {
+        angle++;
+        if(angle > 359){
+            angle = 0;
+        }
+        $("body").css("background-image", "linear-gradient(" + angle + "deg, #7f87a6, #fde7d2)");
+        setTimeout(gradient, speed);
+    }
+    gradient();
+});
+// Get input value
+$(document).on('input', '#range', function() {
+    speed = $(this).val() * 10;
+});
